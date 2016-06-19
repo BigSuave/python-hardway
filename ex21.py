e26dd5313e51bbbd8ccc93903d877d411bdcbbe9
bigsuave <daviddcchatman@gmail.com>
2016-06-18 23:04:53 -0700

    Ex21 I am more in sync with whats happeing but this class is 8weeks so I have do have time to learn but hopefully when I start using the book I become pretty nasty with this shit

---
+++ ex21.py
@@ -0,0 +1,26 @@
+def add(a, b):
+	print "ADDING %d + %d" % (a, b)
+	return a + b
+
+def subtract(a, b):
+	print "SUBTRACTION %d - %d" % (a, b)
+	return a - b
+
+def multiply(a, b):
+	print "MULTITASKING %d * %d" % (a, b)
+	return a * b
+
+def divide(a, b):
+	print "DIVIDING %d / %d" % (a, b)
+	return a / b
+
+print "Let's do some math just functions!"
+
+age = add(30, 5 )
+height = subtract(70, 4)
+weight = multiply(30, 2)
+iq = divide(100, 2)
+
+print "Age: %d, Height: %d, Weight: %d, IQ: %d" % (age, height, weight, iq)
+
+# A puzzle for the extra credit, type it in anyway do later.  
